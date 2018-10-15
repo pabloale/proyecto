595621e16c179362c72405f99d40d3898041fdec
pabloale <pabloale_1302@hotmail.com>
2018-10-15 20:22:08 -0300

    Functions
    	Se hace que las funciones de lectura de los sensores de presion y de distancia trabajen continuamente, especialmente para que sean un thread que corra por siempre y no frene la ejecucion del thread principal.
    	Se agrega un tiempo basico para todos los sensores de espera entre toma de una lectura y otra
    	Se agrega las lineas faltantes del codigo de lectura para los sensores de distancia
    MainThread
    	Se agrega una señal de escucha de cuando se frena el programa para la limpieza de la configuracion de los pines de los sensores
    	Se inicializa una unica vez cada uno de los thread de lectura de cada sensor especifico, evitando que se este creando y destruyendo un thread para cada lectura
    	Se encola los datos de la lectura de los sensores unicamente si se detecta que hay alguien sentado
    	Se usa la configuracion de los actuadores desde la configuracion general y no de cada lectura como antes
    	Se agrega un tiempo basico para todos los sensores de espera entre toma de una lectura y otra
    
    FALTANTES
    	El thread de bluetooth tiene q relanzarse si desconecto
    	El bluetooth debe manejar estados de conexion
    	Guardar en algun lado los datos de la configuracion de actuadores y peso, para que la proxima vez q corra tome la misma configuracion.
    	Si no hay nadie sentado no encolar datos para ser enviados x bluetooth
    	Agregar logica para que la lectura de sensores sea cada X tiempo, especificamente para poder informar una mala postura constante y no ocasional
    	Separar los thread de lecturas de sensores (especialmente de distancia) del thread principal para q los mismos si fallan no corten la ejecucion del programa completo
    	Agregar modificaciones del codigo nuevo al metodo de lectura del sensor distancia
    	Hacer un scketch para que levante automaticamente el script MainThread
    	Limpieza de codigo (prints, variables de parametros, configuraciones y revision general)

--- Functions.py
+++ Functions.py
@@ -58,97 +58,110 @@ def moduloBluetooth():
 def readFuerzaResist(adcnum, clockpin, mosipin, misopin, cspin, index):
     
     global CANT_MUESTRAS_SENSOR_DIST
+    global TIEMPO_ENTRE_ENVIOS_BLUE
     global lecturas_sensores
-    h = 0
-    resultado = 0
-    
-    while (h < CANT_MUESTRAS_SENSOR_DIST):
-        if ((adcnum > 7) or (adcnum < 0)):
-            return -1
-        GPIO.output(cspin, True)
-        GPIO.output(clockpin, False) #start clock low
-        GPIO.output(cspin, False) #bring CS low
 
-        commandout = adcnum
-        commandout |= 0x18 #start bit + single-ended bit
-        commandout <<= 3
+    while True:
+        h = 0
+        resultado = 0
+        while (h < CANT_MUESTRAS_SENSOR_DIST):
+            if ((adcnum > 7) or (adcnum < 0)):
+                return -1
+            GPIO.output(cspin, True)
+            GPIO.output(clockpin, False) #start clock low
+            GPIO.output(cspin, False) #bring CS low
 
-        for i in range(5):
-            if (commandout & 0x80):
-                GPIO.output(mosipin, True)
-            else:
-                GPIO.output(mosipin, False)
-            commandout <<= 1
-            GPIO.output(clockpin, True)
-            GPIO.output(clockpin, False)
+            commandout = adcnum
+            commandout |= 0x18 #start bit + single-ended bit
+            commandout <<= 3
+
+            for i in range(5):
+                if (commandout & 0x80):
+                    GPIO.output(mosipin, True)
+                else:
+                    GPIO.output(mosipin, False)
+                commandout <<= 1
+                GPIO.output(clockpin, True)
+                GPIO.output(clockpin, False)
+                    
+            adcout = 0
+
+            # read in one empty bit, one null bit and 10 ADC bits
+            for i in range(12):
+                GPIO.output(clockpin, True)
+                GPIO.output(clockpin, False)
+                adcout <<= 1
+                if (GPIO.input(misopin)):
+                    adcout |= 0x1
                 
-        adcout = 0
+            GPIO.output(cspin, True)
+            h = h+1
 
-        # read in one empty bit, one null bit and 10 ADC bits
-        for i in range(12):
-            GPIO.output(clockpin, True)
-            GPIO.output(clockpin, False)
-            adcout <<= 1
-            if (GPIO.input(misopin)):
-                adcout |= 0x1
-            
-        GPIO.output(cspin, True)
-        h = h+1
-
-        adcout >>= 1
-        resultado = resultado + adcout
-        #print("actual: ", adcnum, adcout)
-        #print("total: ", adcnum, resultado)
-    
-    lecturas_sensores[index] = resultado / CANT_MUESTRAS_SENSOR_DIST
+            adcout >>= 1
+            resultado = resultado + adcout
+            #print("actual: ", adcnum, adcout)
+            #print("total: ", adcnum, resultado)
+        
+        lecturas_sensores[index] = resultado / CANT_MUESTRAS_SENSOR_DIST
+        time.sleep(TIEMPO_ENTRE_ENVIOS_BLUE)
     
     return resultado / CANT_MUESTRAS_SENSOR_DIST
 
 def readDistance(triggerpin, echopin, topeLectura, index):
 
+    global TIEMPO_ENTRE_ENVIOS_BLUE
     global lecturas_distancia
     distance = 0
     
     VEL_ULTRASONIDO = 34300 #34300 cm/s
     
-    while distance <= 0 or distance > topeLectura:
-        
-        GPIO.setup(triggerpin, GPIO.OUT)
-        GPIO.setup(echopin, GPIO.IN)
-        
-        GPIO.output(triggerpin, GPIO.LOW)
-        
-        #print("esperando que el sensor se estabilice")
-        
-        #time.sleep(2)
-        
-        #print("calculando distancia")
-        
-        GPIO.output(triggerpin, GPIO.HIGH)
-        
-        time.sleep(0.00001)
-        
-        GPIO.output(triggerpin, GPIO.LOW)
-        
-        #print("por calcular el tiempo de respuesta")
-        
-        pulse_start_time = 0
-        pulse_end_time = 0
-        
-        while GPIO.input(echopin)==0:
+    # set GPIO input and output channels
+    GPIO.setup(triggerpin, GPIO.OUT)
+    GPIO.setup(echopin, GPIO.IN)
+    
+    while True:
+        distance = 0
+        while distance <= 0 or distance > topeLectura:
+            
+            GPIO.output(triggerpin, GPIO.LOW)
+            #print("esperando que el sensor se estabilice")
+            #time.sleep(2)
+            time.sleep(0.00001)
+            #print("calculando distancia")
+            
+            # set Trigger to HIGH
+            GPIO.output(triggerpin, GPIO.HIGH)
+            # set Trigger after 0.01ms to LOW
+            time.sleep(0.00001)
+            GPIO.output(triggerpin, GPIO.LOW)
+            
+            #print("por calcular el tiempo de respuesta")
+            
             pulse_start_time = time.time()
-            #print("while PIN_ECHO = 0")
-        while GPIO.input(echopin)==1:
             pulse_end_time = time.time()
-            #print("while PIN_ECHO = 1")
+            
+            # save start time
+            while GPIO.input(echopin)==0:
+                pulse_start_time = time.time()
+                #print("while PIN_ECHO = 0")
+            # save time of arrival
+            while GPIO.input(echopin)==1:
+                pulse_end_time = time.time()
+                #print("while PIN_ECHO = 1")
+            
+            #print("tiempo de respuesta calculado")
+            # time difference between start and arrival
+            pulse_duration = pulse_end_time - pulse_start_time
+            # multiply with the sonic speed (34300 cm/s) and divide by 2, because there and back
+            distance = round(pulse_duration * VEL_ULTRASONIDO / 2, 2)
+            #print("Distance de ", index, ": ", distance, "cm")
+            time.sleep(TIEMPO_ENTRE_ENVIOS_BLUE/2)
         
-        #print("tiempo de respuesta calculado")
-        
-        pulse_duration = pulse_end_time - pulse_start_time
-        distance = round(pulse_duration * VEL_ULTRASONIDO / 2, 2)
-        #print("Distance:", distance, "cm")
-        
-    lecturas_distancia[index] = distance
+        #print("seteo distancia", index)
+        lecturas_distancia[index] = distance
+        #print("sleep", index)
+        time.sleep(TIEMPO_ENTRE_ENVIOS_BLUE)
+        #print("fin sleep", index)
     
     return distance
 
--- MainThread.py
+++ MainThread.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python
 
 import RPi.GPIO as GPIO
+import signal
+import sys
 import time
 import multiprocessing
 from multiprocessing.pool import ThreadPool
 
 from Classes import DataSensores
 from Functions import moduloBluetooth, readFuerzaResist, readDistance, activarActuadores, lecturas_distancia, lecturas_sensores, dataSensoresCollection
+
+def close(signal, frame):
+    print("\nLimpiando la configuracion de pines...\n")
+    GPIO.cleanup()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, close)
 
 #Configuracion de pines de la placa
 SPICLK = 11 #BCM GPIO 11 // BOARD PIN 23
@@ -34,6 +43,7 @@ LED_ROJO_IZQ = 26 #BCM GPIO 26 // BOARD PIN 37
 LED_ROJO_DER = 20 #BCM GPIO 20 // BOARD PIN 38
 VIBRADOR = 21 #BCM GPIO 21 // BOARD PIN 40
 
+# use Raspberry Pi board pin numbers
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
@@ -61,48 +71,51 @@ tolerancia = 5
 pool = ThreadPool(processes=7)
 DEBUG = 1
 
+lectura_distancia_abajo_async = pool.apply_async(readDistance, (DISTANCE_PIN_TRIGGER_ABAJO, DISTANCE_PIN_ECHO_ABAJO, TOPE_LECTURA_ABAJO, 0))
+lectura_distancia_arriba_async = pool.apply_async(readDistance, (DISTANCE_PIN_TRIGGER_ARRIBA, DISTANCE_PIN_ECHO_ARRIBA, TOPE_LECTURA_ARRIBA, 1))
+#retorno_distancia_abajo = lectura_distancia_abajo_async.get()
+#retorno_distancia_arriba = lectura_distancia_arriba_async.get()
+
+lectura_actual_inferior_izquierdo = pool.apply_async(readFuerzaResist, (SENSOR_FUERZA_INFERIOR_IZQ, SPICLK, SPIMOSI, SPIMISO, SPICS, 0))
+lectura_actual_superior_izquierdo = pool.apply_async(readFuerzaResist, (SENSOR_FUERZA_SUPERIOR_IZQ, SPICLK, SPIMOSI, SPIMISO, SPICS, 1))
+lectura_actual_inferior_derecho = pool.apply_async(readFuerzaResist, (SENSOR_FUERZA_INFERIOR_DER, SPICLK, SPIMOSI, SPIMISO, SPICS, 2))
+lectura_actual_superior_derecho = pool.apply_async(readFuerzaResist, (SENSOR_FUERZA_SUPERIOR_DER, SPICLK, SPIMOSI, SPIMISO, SPICS, 3))
+# retorno_inferior_izquierdo = lectura_actual_inferior_izquierdo.get()
+# retorno_superior_izquierdo = lectura_actual_superior_izquierdo.get()
+# retorno_inferior_derecho = lectura_actual_inferior_derecho.get()
+# retorno_superior_derecho = lectura_actual_superior_derecho.get()
+
 pool.apply_async(moduloBluetooth)
 
 #proceso principal
 while True:
-    lectura_cambio = False
-    
-    lectura_actual_inferior_izquierdo = pool.apply_async(readFuerzaResist, (SENSOR_FUERZA_INFERIOR_IZQ, SPICLK, SPIMOSI, SPIMISO, SPICS, 0))
-    lectura_actual_superior_izquierdo = pool.apply_async(readFuerzaResist, (SENSOR_FUERZA_SUPERIOR_IZQ, SPICLK, SPIMOSI, SPIMISO, SPICS, 1))
-    lectura_actual_inferior_derecho = pool.apply_async(readFuerzaResist, (SENSOR_FUERZA_INFERIOR_DER, SPICLK, SPIMOSI, SPIMISO, SPICS, 2))
-    lectura_actual_superior_derecho = pool.apply_async(readFuerzaResist, (SENSOR_FUERZA_SUPERIOR_DER, SPICLK, SPIMOSI, SPIMISO, SPICS, 3))
-    
-    #lectura_distancia_abajo_async = pool.apply_async(readDistance, (DISTANCE_PIN_TRIGGER_ABAJO, DISTANCE_PIN_ECHO_ABAJO, TOPE_LECTURA_ABAJO, 0))
-    #lectura_distancia_arriba_async = pool.apply_async(readDistance, (DISTANCE_PIN_TRIGGER_ARRIBA, DISTANCE_PIN_ECHO_ARRIBA, TOPE_LECTURA_ARRIBA, 1))
-    
-    # retorno_inferior_izquierdo = lectura_actual_inferior_izquierdo.get()
-    # retorno_superior_izquierdo = lectura_actual_superior_izquierdo.get()
-    # retorno_inferior_derecho = lectura_actual_inferior_derecho.get()
-    # retorno_superior_derecho = lectura_actual_superior_derecho.get()
     retorno_inferior_izquierdo = lecturas_sensores[0]
     retorno_superior_izquierdo = lecturas_sensores[1]
     retorno_inferior_derecho = lecturas_sensores[2]
     retorno_superior_derecho = lecturas_sensores[3]
     
-    #retorno_distancia_abajo = lectura_distancia_abajo_async.get()
-    #retorno_distancia_arriba = lectura_distancia_arriba_async.get()
     retorno_distancia_abajo = lecturas_distancia[0]
     retorno_distancia_arriba = lecturas_distancia[1]
     
     dataSensores = DataSensores(retorno_superior_izquierdo, retorno_superior_derecho, retorno_inferior_izquierdo, retorno_inferior_derecho, retorno_distancia_abajo, retorno_distancia_arriba, dataSensoresCollection.getConfig().getPeso())
     
     #dataSensores.imprimirData()
-    
-    #print(dataSensores.concatenarData())
-    
-    dataSensoresCollection.append(dataSensores)
+    print(dataSensores.concatenarData())
     
     if (dataSensores.noHayNadieSentado()):
-        activarActuadores(LED_VERDE_IZQ, LED_VERDE_DER, LED_ROJO_IZQ, LED_ROJO_DER, VIBRADOR, dataSensores.getConfigActuadorVibrador(), dataSensores.getConfigActuadorLed(), GPIO.LOW, GPIO.LOW)
+        #dataSensoresCollection.append(dataSensores)
+        
+        activarActuadores(LED_VERDE_IZQ, LED_VERDE_DER, LED_ROJO_IZQ, LED_ROJO_DER, VIBRADOR, dataSensoresCollection.getConfig().getConfigActuadorVibrador(), dataSensoresCollection.getConfig().getConfigActuadorLed(), GPIO.LOW, GPIO.LOW)
     elif (dataSensores.bienSentado()):
-        activarActuadores(LED_VERDE_IZQ, LED_VERDE_DER, LED_ROJO_IZQ, LED_ROJO_DER, VIBRADOR, dataSensores.getConfigActuadorVibrador(), dataSensores.getConfigActuadorLed(), GPIO.HIGH, GPIO.LOW)
+        dataSensoresCollection.append(dataSensores)
+        
+        activarActuadores(LED_VERDE_IZQ, LED_VERDE_DER, LED_ROJO_IZQ, LED_ROJO_DER, VIBRADOR, dataSensoresCollection.getConfig().getConfigActuadorVibrador(), dataSensoresCollection.getConfig().getConfigActuadorLed(), GPIO.HIGH, GPIO.LOW)
     else: #algun sensor no esta activado, no está bien sentado
-        activarActuadores(LED_VERDE_IZQ, LED_VERDE_DER, LED_ROJO_IZQ, LED_ROJO_DER, VIBRADOR, dataSensores.getConfigActuadorVibrador(), dataSensores.getConfigActuadorLed(), GPIO.LOW, GPIO.HIGH)
+        dataSensoresCollection.append(dataSensores)
+        
+        activarActuadores(LED_VERDE_IZQ, LED_VERDE_DER, LED_ROJO_IZQ, LED_ROJO_DER, VIBRADOR, dataSensoresCollection.getConfig().getConfigActuadorVibrador(), dataSensoresCollection.getConfig().getConfigActuadorLed(), GPIO.LOW, GPIO.HIGH)
+    
+    time.sleep(0.5)
 
 GPIO.cleanup()
 time.sleep(0.5)
